ed985ac8337b49b3c5e55a1c550196754b47536f
yousefalattal <yousefmohnad984@gmail.com>
2026-07-16 17:19:11 +0300

    first task

---
+++ GroupForm.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("===== Group 1 =====")
+    println("Leader = Yousef")
+    println("Team Member 1 = ")
+    println("Team Member 2 = ")
+    println("Team Member 3 = ")
+    println("Team Member 4 = ")
+    println("Team Member 4 = ")
+
+}
