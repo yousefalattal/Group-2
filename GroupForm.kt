60f31d378c3fb589491133532dc599603ef09323
abdel rahman melhem <aboodmelhem935@gmail.com>
2026-07-18 14:40:56 +0300

    😁

--- GroupForm.kt
+++ GroupForm.kt
@@ -1,7 +1,7 @@
 fun main() {
     println("===== Group 1 =====")
     println("Leader = Yousef")
-    println("Team Member 1 =Abdel Rahman Milhem ")
+    println("Team Member 1 = ")
     println("Team Member 2 = ")
     println("Team Member 3 = ")
     println("Team Member 4 = ")
