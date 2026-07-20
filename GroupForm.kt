c060cbbcc9602511e9b1f89a5924e6153d61a3c5
yousef alattal <yousefmohnad984@gmail.com>
2026-07-20 10:26:40 +0300

    Update GroupForm.kt

--- GroupForm.kt
+++ GroupForm.kt
@@ -2,7 +2,7 @@ fun main() {
     println("===== Group 1 =====")
     println("Leader = Yousef")
     println("Team Member 1 = ")
-    println("Team Member 2 = fahed")
+    println("Team Member 2 = ")
     println("Team Member 3 = ")
     println("Team Member 4 = ")
     println("Team Member 5 = ")
